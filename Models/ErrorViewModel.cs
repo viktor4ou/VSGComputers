95cfe491cebba52643a45c58fa7a944c40c0e983
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-08-07 17:19:55 +0300

    initial commit

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace VSGComputers.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
