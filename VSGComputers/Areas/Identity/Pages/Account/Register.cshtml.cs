312f8783e86e597dac7c9d8921423b963b74868b
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-09-01 17:55:48 +0300

    Change on registration to create application user instead of identity user

--- VSGComputers/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ VSGComputers/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -18,6 +18,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using VSGComputers.Models.Models;
 
 namespace VSGComputers.Areas.Identity.Pages.Account
 {
@@ -154,11 +155,11 @@ namespace VSGComputers.Areas.Identity.Pages.Account
             return Page();
         }
 
-        private IdentityUser CreateUser()
+        private ApplicationUser CreateUser()
         {
             try
             {
-                return Activator.CreateInstance<IdentityUser>();
+                return Activator.CreateInstance<ApplicationUser>();
             }
             catch
             {
